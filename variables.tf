438bdff80cc288c9120ee34fe05fde78d7d9d008
Mahesh Ganji <mahesh9.storage@gmail.com>
2024-12-18 16:49:16 +0530

    terraform files

---
+++ variables.tf
@@ -0,0 +1,26 @@
+
+## 
+# Variables
+##
+
+variable "location" {
+    type = string
+    default = "South India"
+}
+
+variable "naming_prefix" {
+  type    = string
+  default = "techielassgha"
+}
+
+variable "tag_environment" {
+  type    = string
+  default = "test"
+}
+
+variable "tag_owner" {
+  type    = string
+  default = "sarah"
+}
+
+
